41df8c7940456116c2db1eaac6ec629011f44e9f
Dmitry Zolotukhin <zlogic@gmail.com>
2012-11-05 00:15:51 +0400

    Java FX is now initialized from fxml. Misc minor improvements.

---
+++ vogon-ui/src/main/java/org/zlogic/vogon/ui/SampleController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.zlogic.vogon.ui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author Dmitry
+ */
+public class SampleController implements Initializable {
+	
+	@FXML
+	private Label label;
+	
+	@FXML
+	private void handleButtonAction(ActionEvent event) {
+		System.out.println("You clicked me!");
+		label.setText("Hello World!");
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		// TODO
+	}	
+}
