5cf0f76b2e7440341b821b9bf0ce0f9e2d88ed89
Dmitry Zolotukhin <zlogic@gmail.com>
2012-11-05 00:15:51 +0400

    Java FX is now initialized from fxml. Misc minor improvements.

--- vogon-ui/src/main/java/org/zlogic/vogon/ui/MainWindow.java
+++ vogon-ui/src/main/java/org/zlogic/vogon/ui/MainWindow.java
@@ -5,11 +5,9 @@
 package org.zlogic.vogon.ui;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -18,24 +16,13 @@ import javafx.stage.Stage;
  */
 public class MainWindow extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		Button btn = new Button();
-		btn.setText("Say 'Hello World'");
-		btn.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				System.out.println("Hello World!");
-			}
-		});
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
 		
-		StackPane root = new StackPane();
-		root.getChildren().add(btn);
+		Scene scene = new Scene(root);
 		
-		Scene scene = new Scene(root, 300, 250);
-		
-		primaryStage.setTitle("Hello World!");
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		stage.setScene(scene);
+		stage.show();
 	}
 
 	/**
