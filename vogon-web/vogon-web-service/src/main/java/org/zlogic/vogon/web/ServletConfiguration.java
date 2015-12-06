50b9ee84f8503ebb784956923e9bfca1fb409199
Dmitry Zolotukhin <zlogic@gmail.com>
2015-12-06 19:46:15 +0300

    Bumped up artifact versions to 2.2-SNAPSHOT. Migrated to Spring Boot 1.3, while retaining compatibility with Tomcat 7 by configuring Tomcat via reflection. Bumped up dependency versions.

--- vogon-web/vogon-web-service/src/main/java/org/zlogic/vogon/web/ServletConfiguration.java
+++ vogon-web/vogon-web-service/src/main/java/org/zlogic/vogon/web/ServletConfiguration.java
@@ -5,18 +5,9 @@
  */
 package org.zlogic.vogon.web;
 
-import java.io.File;
-import java.text.MessageFormat;
-import java.util.ResourceBundle;
-import org.apache.catalina.connector.Connector;
-import org.apache.coyote.http11.AbstractHttp11JsseProtocol;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.embedded.MimeMappings;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -26,26 +17,6 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class ServletConfiguration implements EmbeddedServletContainerCustomizer {
-
-	/**
-	 * The logger
-	 */
-	private final static Logger log = LoggerFactory.getLogger(ServletConfiguration.class);
-	/**
-	 * Localization messages
-	 */
-	private static final ResourceBundle messages = ResourceBundle.getBundle("org/zlogic/vogon/web/messages");
-	/**
-	 * Keystore file
-	 */
-	@Value("${vogon.keystore.file:}")
-	private String keystoreFile;
-	/**
-	 * Keystore password
-	 */
-	@Value("${vogon.keystore.pass:}")
-	private String keystorePass;
-
 	/**
 	 * Customizes the ConfigurableEmbeddedServletContainer by adding extra MIME
 	 * types
@@ -58,38 +29,5 @@ public class ServletConfiguration implements EmbeddedServletContainerCustomizer 
 		mappings.add("woff", "application/font-woff"); //NOI18N
 		mappings.add("eot", "application/vnd.ms-fontobject"); //NOI18N
 		container.setMimeMappings(mappings);
-		if (container instanceof TomcatEmbeddedServletContainerFactory) {
-			TomcatEmbeddedServletContainerFactory tomcatContainer = ((TomcatEmbeddedServletContainerFactory) container);
-			tomcatContainer.setUriEncoding("utf-8"); //NOI18N
-			configureSSL(tomcatContainer);
-		}
-	}
-
-	/**
-	 * Configures SSL for Tomcat container
-	 *
-	 * @param container TomcatEmbeddedServletContainerFactory instance to
-	 * configure
-	 */
-	private void configureSSL(TomcatEmbeddedServletContainerFactory container) {
-		if (keystoreFile.isEmpty() || keystorePass.isEmpty()) {
-			log.debug(messages.getString("KEYSTORE_FILE_OR_PASSWORD_NOT_DEFINED"));
-			return;
-		}
-		log.error(MessageFormat.format(messages.getString("USING_KEYSTORE_WITH_PASSWORD"), new Object[]{keystoreFile, keystorePass.replaceAll(".{1}", "*")})); //NOI18N
-		Connector connector = new Connector();
-		connector.setPort(8443);
-		connector.setSecure(true);
-		connector.setScheme("https"); //NOI18N
-		connector.setURIEncoding(container.getUriEncoding());
-
-		AbstractHttp11JsseProtocol proto = (AbstractHttp11JsseProtocol) connector.getProtocolHandler();
-		proto.setSSLEnabled(true);
-		proto.setKeystoreFile(new File(keystoreFile).getAbsolutePath());
-		proto.setKeystorePass(keystorePass);
-		proto.setKeystoreType("JKS"); //NOI18N
-		proto.setKeyAlias("vogonkey"); //NOI18N
-
-		container.addAdditionalTomcatConnectors(connector);
 	}
 }
