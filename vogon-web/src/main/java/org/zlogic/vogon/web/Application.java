bf086572f075600410dd980c96568a981737a21e
Dmitry Zolotukhin <zlogic@gmail.com>
2017-02-11 20:36:43 +0100

    Bumped up dependency versions. Resolved compatibility issues.

--- vogon-web/src/main/java/org/zlogic/vogon/web/Application.java
+++ vogon-web/src/main/java/org/zlogic/vogon/web/Application.java
@@ -8,7 +8,7 @@ package org.zlogic.vogon.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
